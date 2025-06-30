15ec6df5dfc6f43c6eea9de7893ab5f50d21aa88
techenthusiast321 <12212140@nitkkr.ac.in>
2025-07-01 00:54:16 +0530

    frontend completed

---
+++ src/components/admin/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col border-r border-gray-200 min-h-full pt-6">
+        <NavLink end={true} to='/admin' className={({isActive})=>isActive ? 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 bg-primary/10 border-r-4 border-primary cursor-pointer' : 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 rounded-full cursor-pointer'}>
+            <img src={assets.home_icon} alt="" className="min-w-4 w-5"/>
+            <p className="hidden md:inline-block">Dashboard</p>
+        </NavLink>
+
+        <NavLink end={true} to='/admin/addBlog' className={({isActive})=>isActive ? 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 bg-primary/10 border-r-4 border-primary cursor-pointer' : 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 rounded-full cursor-pointer'}>
+            <img src={assets.add_icon} alt="" className="min-w-4 w-5"/>
+            <p className="hidden md:inline-block">Add blogs</p>
+        </NavLink>
+
+        <NavLink end={true} to='/admin/listBlog' className={({isActive})=>isActive ? 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 bg-primary/10 border-r-4 border-primary cursor-pointer' : 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 rounded-full cursor-pointer'}>
+            <img src={assets.list_icon} alt="" className="min-w-4 w-5"/>
+            <p className="hidden md:inline-block">Blog Lists</p>
+        </NavLink>
+
+        <NavLink end={true} to='/admin/comments' className={({isActive})=>isActive ? 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 bg-primary/10 border-r-4 border-primary cursor-pointer' : 'flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 rounded-full cursor-pointer'}>
+            <img src={assets.comment_icon} alt="" className="min-w-4 w-5"/>
+            <p className="hidden md:inline-block">Comments</p>
+        </NavLink>
+    </div>
+  )
+}
+
+export default Sidebar
