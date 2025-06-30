6e4462918cc7cfe3fa3b3f5b5871038d71631654
techenthusiast321 <12212140@nitkkr.ac.in>
2025-07-01 01:11:29 +0530

    Blog author name change

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -75,7 +75,7 @@ const Blog = () => {
             <p className="text-primary py-4 font-medium">Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
             <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">{data.title}</h1>
             <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
-            <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">Michael Brown</p>
+            <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">Pranjal Arya</p>
           </div>
 
           <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
